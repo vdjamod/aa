01ccbe1b9bdf6487e675dc2b47dd1a3543082ecc
vipuljamod <vipuljamod122@gamil.com>
2024-09-03 13:39:55 +0530

    scommit

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ const mp4_to_text = require('./Routes/mp4_to_text')
 const generate_pdf = require('./generate_pdf')
 const user = require('./Routes/users')
 const pdf = require('./Routes/addPdf')
+const app = require('./backend/React-Pdf-Multer-Backend/app')
 
 
 app.use(cors());
+
 app.use("/files", express.static("files"))
 app.use(bodyParser.json());
 app.use('/youtube',youtube_search);
@@ -26,7 +28,8 @@ app.use('/growth',video_growth);
 app.use('/',generate_pdf);
 app.use('/',mp4_to_text);
 app.use('/user',user); 
-app.use('/pdf',pdf); 
+app.use('/pdf',pdf);
+app.use('/backend',app); 
 app.get('/',(req,res)=>{
     res.json({msg:"Welcome, Welcome, Bhale Padhara"});
 })
